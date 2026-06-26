0f8d7df9ed2f92aea93a7019cc9d5c663e5c23a8
Mordecai Yiadom <mordeyiadom@gmail.com>
2026-06-26 02:18:39 -0400

    Implement localized channel testing

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 #load discord bot token from .env file
 load_dotenv()
 BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
 if(BOT_TOKEN): 
     print("INFO discord token");
+
+TESTING_CHANNEL_NAME = "mordbot-playground"
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -16,8 +19,8 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
-        if(message.author != self.user): 
-            await message.channel.send(message.content);
+        if((message.author != self.user) and message.channel.name == TESTING_CHANNEL_NAME):
+            await message.channel.send("https://tenor.com/view/sonic-devil-diabolique-evil-gif-9725651736562738158")
 
 
 intents = discord.Intents.default()
